2b6707c4a682f9b236cd3bd34e60c1dfaae7dff9
Bekasith <bekasith@hotmail.com>
2017-01-30 12:33:39 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = '';
+$access_token = 'Ns2HTJkXtLxaV0E52ZI409E6oG/NVwN7ZKXTaHZSs/KS4LenNVh6VpCiz+AwRpTHqlUH9fw+iJRxWULG7LHdeIEmoNT67iR3AswlFGJJO6W7et3YixhBF5gCQDgtG/Idq08FdSHYS9OMOBQwdqOQxQdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
