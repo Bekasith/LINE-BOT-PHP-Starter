6f10c40bd3f1424896849db5745f33c498138890
Bekasith <bekasith@hotmail.com>
2017-01-30 12:59:00 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -12,14 +12,14 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			$text = $event['message']['text']+' I lov U';
+			$text = $event['message']['text'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => $text +' I lov U'
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
