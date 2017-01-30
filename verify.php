0176e80ce09e77d45a1a304182adccba0c903bbf
Bekasith <bekasith@hotmail.com>
2017-01-30 12:24:26 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,7 +1,7 @@
 <?php 
    $access_token = 'Ns2HTJkXtLxaV0E52ZI409E6oG/NVwN7ZKXTaHZSs/KS4LenNVh6VpCiz+AwRpTHqlUH9fw+iJRxWULG7LHdeIEmoNT67iR3AswlFGJJO6W7et3YixhBF5gCQDgtG/Idq08FdSHYS9OMOBQwdqOQxQdB04t89/1O/w1cDnyilFU=';
    
-   $url = 'https://api.line.me/v1oauth/verify';
+   $url = 'https://api.line.me/v1/oauth/verify';
    
    $headers = array(Authorization: Bearer ' . $access_token);
    
