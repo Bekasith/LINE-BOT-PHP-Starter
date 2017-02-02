6e2b43c4d4c4509fa0f1fc138e46054fef889a60
Bekasith <bekasith@hotmail.com>
2017-02-02 14:11:37 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -7,8 +7,9 @@ $response = curl_exec($ch);
 curl_close($ch);
 
 var_dump($response);
-
 // URL-Xray 
+
+echo file_get_contents('http://180.183.251.233:4444/');
 
 $access_token = 'Ns2HTJkXtLxaV0E52ZI409E6oG/NVwN7ZKXTaHZSs/KS4LenNVh6VpCiz+AwRpTHqlUH9fw+iJRxWULG7LHdeIEmoNT67iR3AswlFGJJO6W7et3YixhBF5gCQDgtG/Idq08FdSHYS9OMOBQwdqOQxQdB04t89/1O/w1cDnyilFU=';
 
