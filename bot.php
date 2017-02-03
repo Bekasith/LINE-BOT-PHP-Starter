ec55a21ad8c3cc6ae664f7660f9a01f592de6856
Bekasith <bekasith@hotmail.com>
2017-02-03 12:11:14 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -40,28 +40,28 @@ if (!is_null($events['events'])) {
 	Fabric	79	79 ผ้าอื่น ๆ 	79		
 */		
 //	$res = 'ม่านปรับแสง'.' รหัส'.$code.' คงเหลือ'.$bal.' จอง['.$reserve.'] เมื่อ '.$update   // every text return from myHost
-			switch ($gdtype) {
-			    case "5":  
+		switch ($gdtype) {
+			case "5":  
 			     $res = 'ม่านม้วน'.' รหัส'.$code.' คงเหลือ'.$bal.' จอง['.$reserve.'] เมื่อ '.$update; break;
-			    case "6":
+			case "6":
 			     $res = 'ม่านปรับแสง'.' รหัส'.$code.' คงเหลือ'.$bal.' จอง['.$reserve.'] เมื่อ '.$update; break;
-			    case "72"
+			case "72" :
 			     $res = 'ผ้าโปร่ง'.' รหัส'.$code.' คงเหลือ'.$bal.' จอง['.$reserve.'] เมื่อ '.$update; break;
-			    case "73"
+			case "73" :
 			     $res = 'ผ้า Italy '.' รหัส'.$code.' คงเหลือ'.$bal.' จอง['.$reserve.'] เมื่อ '.$update; break;
-			    case "74"
+			case "74" :
 			     $res = 'ผ้า Blckout '.' รหัส'.$code.' คงเหลือ'.$bal.' จอง['.$reserve.'] เมื่อ '.$update; break;
-			    case "75"
+			case "75" :
 			     $res = 'ผ้าไหม'.' รหัส'.$code.' คงเหลือ'.$bal.' จอง['.$reserve.'] เมื่อ '.$update; break;
-			    case "76"
+			case "76" : 
 			     $res = 'ผ้าโซฟา'.' รหัส'.$code.' คงเหลือ'.$bal.' จอง['.$reserve.'] เมื่อ '.$update; break;
-			    case "77"
+			case "77" : 
 			     $res = 'ผ้า รพ.'.' รหัส'.$code.' คงเหลือ'.$bal.' จอง['.$reserve.'] เมื่อ '.$update; break;
-			    case "78"
+			case "78": 
 			     $res = 'ผ้าม่าน'.' รหัส'.$code.' คงเหลือ'.$bal.' จอง['.$reserve.'] เมื่อ '.$update; break;
-			    case"79":
+			case"79":
 			     $res = 'ผ้าม่าน'.' รหัส'.$code.' คงเหลือ'.$bal.' จอง['.$reserve.'] เมื่อ '.$update; break;
-			    default:
+			default:
 				$res = "สินค้านี้ ยังไม่พร้อมให้ ข้อมูล";
 			}			
 			// Build message to reply back
