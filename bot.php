ffdb4c61c273c1f9d001a225abfb7bf54797cbff
Bekasith <bekasith@hotmail.com>
2017-02-03 08:58:50 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -32,7 +32,7 @@ if (!is_null($events['events'])) {
 			$messages = [
 				'type' => 'text',
 //				'text' => $text . $a
-				'text' =>  $a .'ฟหกด่าสว'  // every text return from myHost
+				'text' =>  $a   // every text return from myHost
 
 			];
 				
