9462111595717ab19a9c5a2090b1f2c45a3b5bf5
Bekasith <bekasith@hotmail.com>
2017-02-09 11:31:57 +0700

    Update test-block.php

--- test-block.php
+++ test-block.php
@@ -26,6 +26,7 @@ if (!is_null($events['events'])) {
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
 			$text = $event['message']['text'];
+			$cid = $event['message']['id'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 			$mid = $event['source']['userId'];
@@ -80,7 +81,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' =>  $res
+				'text' =>  $mid.'-'.$cid
 			];
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
