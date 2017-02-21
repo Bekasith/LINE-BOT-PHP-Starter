6af22cad85b0269bbc74cf91ee885a625e5b7af2
Bekasith <bekasith@hotmail.com>
2017-02-21 11:00:18 +0700

    Update getInfo.php

--- getInfo.php
+++ getInfo.php
@@ -2,21 +2,23 @@
 
 $access_token = 'Ns2HTJkXtLxaV0E52ZI409E6oG/NVwN7ZKXTaHZSs/KS4LenNVh6VpCiz+AwRpTHqlUH9fw+iJRxWULG7LHdeIEmoNT67iR3AswlFGJJO6W7et3YixhBF5gCQDgtG/Idq08FdSHYS9OMOBQwdqOQxQdB04t89/1O/w1cDnyilFU=';
 
+/* 
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '084e9260baa38004fc2eb2b599fc5157']);
+$response = $bot->getProfile('U583bfafb13ea45d23ab1bb4c545b2d0a');
+
+*/
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '084e9260baa38004fc2eb2b599fc5157']);
 $response = $bot->getProfile('U583bfafb13ea45d23ab1bb4c545b2d0a');
 
-
-  
-echo "OK1";
-/*if ($response->isSucceeded()) {
+if ($response->isSucceeded()) {
     $profile = $response->getJSONDecodedBody();
     echo $profile['displayName'];
     echo $profile['pictureUrl'];
     echo $profile['statusMessage'];
 }
-*/
 
 echo "OK";
 
