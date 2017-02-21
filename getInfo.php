6436c49f510ffa06227df41afd93ac70fa3239d2
Bekasith <bekasith@hotmail.com>
2017-02-21 11:30:10 +0700

    Update getInfo.php

--- getInfo.php
+++ getInfo.php
@@ -8,17 +8,25 @@ $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '084e9260baa38004fc2eb
 $response = $bot->getProfile('U583bfafb13ea45d23ab1bb4c545b2d0a');
 
 */
+            $url = 'https://api.line.me/v2/bot/profile/U583bfafb13ea45d23ab1bb4c545b2d0a';
+			 
+			$headers = array('Content-Type: application/json; charset=UTF-8', 'Authorization: Bearer ' . $access_token);
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+			echo $result . "\r\n";
 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '084e9260baa38004fc2eb2b599fc5157']);
-$response = $bot->getProfile('U583bfafb13ea45d23ab1bb4c545b2d0a');
-
+/*
 if ($response->isSucceeded()) {
     $profile = $response->getJSONDecodedBody();
     echo $profile['displayName'];
     echo $profile['pictureUrl'];
     echo $profile['statusMessage'];
 }
-
+*/
 echo "OK";
 
