68614cbeee966179d3a7e1127bc288f3d5b3ec1e
Bekasith <bekasith@hotmail.com>
2017-03-04 17:02:22 +0700

    Update info.php

--- info.php
+++ info.php
@@ -8,8 +8,14 @@ $channelSecret = '084e9260baa38004fc2eb2b599fc5157';
 $access_token  = 'Ns2HTJkXtLxaV0E52ZI409E6oG/NVwN7ZKXTaHZSs/KS4LenNVh6VpCiz+AwRpTHqlUH9fw+iJRxWULG7LHdeIEmoNT67iR3AswlFGJJO6W7et3YixhBF5gCQDgtG/Idq08FdSHYS9OMOBQwdqOQxQdB04t89/1O/w1cDnyilFU=';
 
 $bot = new BOT_API($channelSecret, $access_token);
-	
-$bot->sendMessageNew('Uf1a74f917aa3ed3ed98e417fcc209fee', 'Poko ไง จะใครหล่ะ \n เราจะมาบอกว่า ลองถามเราแบบนี้ หรือยัง\n "Roma-" ลองดูสิ  !! ');
+
+//$msg = $msg.""."\n";
+$msg = "Poko ไง จะใครหล่ะ"."\n";
+$msg = $msg."เราจะมาบอกว่า ลองถามเราแบบนี้ หรือยัง"."\n";
+$msg = $msg." 'Roma-' ลองดูสิ  !! "."\n";
+
+
+$bot->sendMessageNew('Uf1a74f917aa3ed3ed98e417fcc209fee', $msg);
 
 if ($bot->isSuccess()) {
 	echo 'Succeeded!';
