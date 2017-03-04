b61ca232de3872fc9c7e582db4552f38c24e290d
Bekasith <bekasith@hotmail.com>
2017-03-04 16:55:30 +0700

    Update info.php

--- info.php
+++ info.php
@@ -2,7 +2,7 @@
 
 // example: https://github.com/onlinetuts/line-bot-api/blob/master/php/example/chapter-02.php
 
-include ('line-bot-api/php/line-bot.php');
+include ('php/line-bot.php');
 
 $channelSecret = '084e9260baa38004fc2eb2b599fc5157';
 $access_token  = 'Ns2HTJkXtLxaV0E52ZI409E6oG/NVwN7ZKXTaHZSs/KS4LenNVh6VpCiz+AwRpTHqlUH9fw+iJRxWULG7LHdeIEmoNT67iR3AswlFGJJO6W7et3YixhBF5gCQDgtG/Idq08FdSHYS9OMOBQwdqOQxQdB04t89/1O/w1cDnyilFU=';
