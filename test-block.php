1e55ad837691e26492eadc084c70c277728fa92f
Bekasith <bekasith@hotmail.com>
2017-03-08 15:21:15 +0700

    Update test-block.php

--- test-block.php
+++ test-block.php
@@ -86,7 +86,9 @@ if (!is_null($events['events'])) {
 			}
 	 
 */			
-			utf8_encode_deep($a);
+			//utf8_encode_deep($a);
+				$a = utf8_encode($a);
+			
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
@@ -114,22 +116,5 @@ if (!is_null($events['events'])) {
 	}
 }
 echo "OK";
-
-function utf8_encode_deep(&$input) {
-    if (is_string($input)) {
-        $input = utf8_encode($input);
-    } else if (is_array($input)) {
-        foreach ($input as &$value) {
-            utf8_encode_deep($value);
-        }
-
-        unset($value);
-    } else if (is_object($input)) {
-        $vars = array_keys(get_object_vars($input));
-
-        foreach ($vars as $var) {
-            utf8_encode_deep($input->$var);
-        }
-    }
-}
+ 
 ?>
