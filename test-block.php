e3f0ef92d89d4e1ed375a2f9a24de63bb26e2ad0
Bekasith <bekasith@hotmail.com>
2017-03-11 10:09:21 +0700

    Update test-block.php

--- test-block.php
+++ test-block.php
@@ -63,7 +63,7 @@ if (!is_null($events['events'])) {
 			$messages = [
 				'type' => 'text',
 // 				'text' =>  $mid.'-'.$cid
-				'text' =>  $a
+				'text' =>  $res
 			];
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
