75792801e17667351b35118c90007eada8830887
Bekasith <bekasith@hotmail.com>
2017-03-11 10:07:49 +0700

    Update test-block.php

--- test-block.php
+++ test-block.php
@@ -31,18 +31,14 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 			$mid = $event['source']['userId'];
 			
-//			$a = file_get_contents('http://180.183.251.233:4444/search.asp?id='.urlencode($text).'&mid='.urlencode($mid));
 			$a = file_get_contents('http://180.183.251.233:4444/searchTest.asp?id='.urlencode($text).'&mid='.urlencode($mid));
-		//	var_dump($text);
+
 			list($gdtype , $code, $bal, $reserve, $info) = split("#", $a, 5);
-	
-//	$res = 'ม่านปรับแสง'.' รหัส'.$code.' คงเหลือ'.$bal.' จอง['.$reserve.'] เมื่อ '.$update   // every text return from myHost
-/*			$res= "";
+			$res= "";
 			switch ($gdtype) {
 			case "-1" :
 			     $res = 'กรุณา ลงทะเบียน ทาง PG@kaceebest.com ด้วย ข้อความนี้ id=['.$mid.'],[ ชื่อ ],[ ลค/พนง ]' ; break;
 			case "0" :
-				 
 				$splittedstring=explode("@",$code);
 				foreach ($splittedstring as $key => $value) {
 				  echo "splittedstring[".$key."] = ".$value."<br>";
@@ -51,45 +47,19 @@ if (!is_null($events['events'])) {
 					break;
 				//$res =  $code; break;
 					
-			case "2":  
-			     $res = 'อลูฯ'.' รหัส '.$code.' คงเหลือ'.$bal.' จอง['.$reserve.']  '.$info; break;
-			case "5":  
-			     $res = 'ม่านม้วน'.' รหัส '.$code.' คงเหลือ'.$bal.' จอง['.$reserve.']  '.$info; break;
-			case "6":
-			     $res = 'ม่านปรับแสง'.' รหัส '.$code.' คงเหลือ'.$bal.' จอง['.$reserve.']  '.$info; break;
-			case "10":
-			     $res = 'รางประดับ'.' รหัส '.$code.' คงเหลือ'.$bal.' จอง['.$reserve.']  '.$info; break;
-			case "40" :
-			     $res = 'วอลล์ '.' รหัส '.$code.' คงเหลือ'.$bal.' จอง['.$reserve.']  '.$info; break;
-			case "50" :
-			     $res = 'สายรวบ'.' รหัส '.$code.' คงเหลือ'.$bal.' จอง['.$reserve.']  '.$info; break;
-			case "51" :
-			     $res = 'ชายครุย'.' รหัส '.$code.' คงเหลือ'.$bal.' จอง['.$reserve.']  '.$info; break;
-			case "72" :
-			     $res = 'ผ้าโปร่ง'.' รหัส '.$code.' คงเหลือ'.$bal.' จอง['.$reserve.']  '.$info; break;
-			case "73" :
-			     $res = 'ผ้า Italy '.' รหัส '.$code.' คงเหลือ'.$bal.' จอง['.$reserve.']  '.$info; break;
-			case "74" :
-			     $res = 'ผ้า Blckout '.' รหัส '.$code.' คงเหลือ'.$bal.' จอง['.$reserve.']  '.$info; break;
-			case "75" :
-			     $res = 'ผ้าไหม'.' รหัส '.$code.' คงเหลือ'.$bal.' จอง['.$reserve.']  '.$info; break;
-			case "76" : 
-			     $res = 'ผ้าโซฟา'.' รหัส '.$code.' คงเหลือ'.$bal.' จอง['.$reserve.']  '.$info; break;
-			case "77" : 
-			     $res = 'ผ้า รพ.'.' รหัส '.$code.' คงเหลือ'.$bal.' จอง['.$reserve.']  '.$info; break;
-			case "78" : 
-			     $res = 'ผ้าม่าน'.' รหัส '.$code.' คงเหลือ'.$bal.' จอง['.$reserve.']  '.$info; break;
-			case "79" :
-			     $res = 'ผ้าม่าน'.' รหัส '.$code.' คงเหลือ'.$bal.' จอง['.$reserve.']  '.$info; break;
+			case "2" :
+				$splittedstring=explode("~",$code);
+				foreach ($splittedstring as $key => $value) {
+				  echo "splittedstring[".$key."] = ".$value."<br>";
+				  $res = $res.$value."\n";
+				} 
+					break;
+				//$res =  $code; break;
 			default:
 			     $res = "สินค้านี้ ยังไม่พร้อมให้ ข้อมูล ";
 			}
 	 
-*/			
-			//utf8_encode_deep($a);
-				//$a = utf8_encode($a);
 			
-			// Build message to reply back
 			$messages = [
 				'type' => 'text',
 // 				'text' =>  $mid.'-'.$cid
